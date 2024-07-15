806ed093777537f8d0844ba5309f0e890506bd1e
Muhammad Ismail Rasheed <159624882+MIR00291341@users.noreply.github.com>
2024-07-15 14:00:35 +0500

    add one more part of assignment
    In new part of assignment make a factorial program use by loop.

---
+++ assignment4B.py
@@ -0,0 +1,7 @@
+num = int(input("Enter Number: ")) #input from user 
+factorial = 1                       #create varible of factor
+for i in range (1,num+1):           #use of loop and plus 1 more number in user input
+
+    factorial= factorial*i          #use factor formula
+print(factorial)                    #print user input in factorial (if user input = 5 output/answer will be 120)
+
